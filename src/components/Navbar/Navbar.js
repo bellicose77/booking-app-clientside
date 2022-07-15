0e86dc56e95005aadfc578850dc1012d315b36d9
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-07-15 16:26:36 +0600

    navigation

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,10 @@ const Navbar = () => {
     return (
         <div className='navbar'>
             <div className='navbar-container'>
-
+              <span></span>
+              <div>
+                <button>Register</button>
+              </div>
             </div>
             
         </div>
