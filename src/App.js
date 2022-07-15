b31a0d55ed54d12b7ad5ac9765a78a8b17e296bd
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-07-15 14:01:05 +0600

    koto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import {BrowserRouter as Router} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home/Home';
 function App() {
   return (
     <div>
      <Router>
-      
+      <Routes>
+        <Route path='' element={<Home/>}
+      </Routes>
      </Router>
     </div>
   );
