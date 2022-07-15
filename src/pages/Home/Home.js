0aae3cf3a10d32a7bc2f854745b00a7d3f17e364
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-07-15 18:38:47 +0600

    header section

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from '../../components/Header/Header';
 import Navbar from '../../components/Navbar/Navbar';
 import './Home.css';
 const Home = () => {
     return (
         <div>
            <Navbar/>
+           <Header/>
         </div>
     );
 };
