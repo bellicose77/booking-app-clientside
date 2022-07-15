60b7b47f00ddb9561aa0a7a3a67dd1fe186d4102
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-07-15 19:00:29 +0600

    header section

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,11 +8,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Header = () => {
     return (
-        <div className='headerlist'>
-           <div>
-           <FontAwesomeIcon icon={faBed} />
-           <span>Stays</span>
-           </div>
+    <div className='header'>
+        <div className='headerList'>
+              <div className='headerListItem'>
+                 <FontAwesomeIcon icon={faBed} />
+                  <span>Stays</span>
+              </div>
+        </div>
+           
            <div>
            <FontAwesomeIcon icon={faPlane} />
            <span>Flights</span>
