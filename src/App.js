cb55defd632c920d6f31da13a95a745d5647ae7c
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-07-15 14:03:06 +0600

    path

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Home from './pages/Home/Home';
+import Hotel from './pages/Hotel/Hotel';
 function App() {
   return (
     <div>
      <Router>
       <Routes>
-        <Route path='' element={<Home/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/hotel' element={<Hotel/>}/>
       </Routes>
      </Router>
     </div>
