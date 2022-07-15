2160b7a8ee6a46c630a0bc8c1b27c00e7fea105e
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-07-15 18:45:18 +0600

    header section

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import {
+    faBed,
+  
+  } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Header = () => {
     return (
         <div>
            <div>
-           <FontAwesomeIcon icon="fa-solid fa-bed" />
+           <FontAwesomeIcon icon={faBed} />
+           <span>Stays</span>
            </div>
         </div>
     );
