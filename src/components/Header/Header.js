929f5887e69ae4e8a45e07d51989586bfd2afade
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-07-16 16:48:08 +0600

    header section done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,6 +8,7 @@ import { DateRange } from 'react-date-range';
 import 'react-date-range/dist/styles.css'; // main css file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { format } from 'date-fns';
 
 const Header = () => {
     const [openDate,setOpenDate]=useState(false)
