f7c28aafbed5e3e45d6edc3eab5d33a10c457ded
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-07-19 00:07:01 +0600

    home page properList

--- src/components/PropertyList/PropertyList.js
+++ src/components/PropertyList/PropertyList.js
@@ -7,8 +7,8 @@ const PropertyList = () => {
                 <img className='pLimg'
                 src='https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o='
                 />
-                <div>
-                    
+                <div className='pLtitle'>
+
                 </div>
             </div>
         </div>
