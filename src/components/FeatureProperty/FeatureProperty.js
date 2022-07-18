fce45b08e59976900ebe6feec7ea3ff97b21ff87
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-07-18 23:55:36 +0600

    home page featured

--- src/components/FeatureProperty/FeatureProperty.js
+++ src/components/FeatureProperty/FeatureProperty.js
@@ -4,7 +4,7 @@ const FeatureProperty = () => {
     return (
         <div className='propertyContainer'>
             <div className='propertyItem'>
-                
+                <img src=''
 
             </div>
             
