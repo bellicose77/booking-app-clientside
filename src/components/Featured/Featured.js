85fcc87ea9a362b8de6c34b150b6add535ab49eb
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-07-18 22:58:52 +0600

    home page featured

--- src/components/Featured/Featured.js
+++ src/components/Featured/Featured.js
@@ -5,7 +5,9 @@ const Featured = () => {
     return (
         <div className='featureContainer'>
             <div className='featureItem'>
-
+              <img src='https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o='
+              alt='noting' className='featureImg'
+              />
             </div>
         </div>
     );
