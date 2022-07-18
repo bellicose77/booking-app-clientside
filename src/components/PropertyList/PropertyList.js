4d421e6ea1897446e0c3e58c2f8024cae71b800b
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-07-19 00:23:37 +0600

    home page properList

--- src/components/PropertyList/PropertyList.js
+++ src/components/PropertyList/PropertyList.js
@@ -30,6 +30,25 @@ const PropertyList = () => {
                    <h2>87042 Vilas</h2>
                 </div>
             </div>
+            <div className='pLitem'>
+                <img className='pLimg'
+                src='https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_resorts/6f87c6143fbd51a0bb5d15ca3b9cf84211ab0884.jpg'
+                />
+                <div className='pLtitle'>
+                   <h1>Vilas</h1>
+                   <h2>87042 Vilas</h2>
+                </div>
+            </div>
+            <div className='pLitem'>
+                <img className='pLimg'
+                src='https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_resorts/6f87c6143fbd51a0bb5d15ca3b9cf84211ab0884.jpg'
+                />
+                <div className='pLtitle'>
+                   <h1>Vilas</h1>
+                   <h2>87042 Vilas</h2>
+                </div>
+            </div>
+            
         </div>
     );
 };
