d152e4b32eef1e9fb4e46b7f06b57d2f5a68b3fd
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-07-18 21:51:39 +0600

    heade

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,8 +7,8 @@ const Home = () => {
         <div>
            <Navbar/>
            <Header/>
-           <div>
-            
+           <div className='homeContainer'>
+
            </div>
         </div>
     );
