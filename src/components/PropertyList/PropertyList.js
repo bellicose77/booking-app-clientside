bf9647030a586428bd399b8186dbc2f3cc87a762
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-07-19 00:10:24 +0600

    home page properList

--- src/components/PropertyList/PropertyList.js
+++ src/components/PropertyList/PropertyList.js
@@ -17,8 +17,8 @@ const PropertyList = () => {
                 src='https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o='
                 />
                 <div className='pLtitle'>
-                   <h1>Hotels</h1>
-                   <h2>87342 hotels</h2>
+                   <h1>Apartments</h1>
+                   <h2>87342 apartment</h2>
                 </div>
             </div>
             <div className='pLitem'>
