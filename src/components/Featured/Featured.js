1135401dddea95bb96c09b2e7050e9fc15107946
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-07-18 23:06:37 +0600

    home page featured

--- src/components/Featured/Featured.js
+++ src/components/Featured/Featured.js
@@ -14,7 +14,7 @@ const Featured = () => {
               </div>
             </div>
             <div className='featureItem'>
-              <img src='https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o='
+              <img src='https://cf.bstatic.com/xdata/images/city/max500/690334.webp?k=b99df435f06a15a1568ddd5f55d239507c0156985577681ab91274f917af6dbb&o='
               alt='noting' className='featureImg'
               />
               <div className='featureText'>
