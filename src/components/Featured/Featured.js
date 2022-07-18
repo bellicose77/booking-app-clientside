9e7007e3c8653e40bf50fc4f2ad99311c6919b5f
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-07-18 23:02:28 +0600

    home page featured

--- src/components/Featured/Featured.js
+++ src/components/Featured/Featured.js
@@ -8,6 +8,9 @@ const Featured = () => {
               <img src='https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o='
               alt='noting' className='featureImg'
               />
+              <div className='featureText'>
+                
+              </div>
             </div>
         </div>
     );
