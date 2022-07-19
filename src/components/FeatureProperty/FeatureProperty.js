1f9bbae9713c4c6f9fc32a8d3cee69e132faf1f4
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-07-19 21:58:23 +0600

    home page featuredProperty

--- src/components/FeatureProperty/FeatureProperty.js
+++ src/components/FeatureProperty/FeatureProperty.js
@@ -4,7 +4,8 @@ const FeatureProperty = () => {
     return (
         <div className='fp'>
             <div className='fpItem'>
-                
+                <img src='https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1'
+                className='fpImg'/>
 
             </div>
             
