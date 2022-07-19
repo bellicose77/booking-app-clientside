a9d31894ba076d4de4f37e0ab4775ec5098861c1
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-07-20 00:23:09 +0600

    mail componet

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Featured from '../../components/Featured/Featured';
 import FeatureProperty from '../../components/FeatureProperty/FeatureProperty';
 import Header from '../../components/Header/Header';
+import MailList from '../../components/MailList/MailList';
 import Navbar from '../../components/Navbar/Navbar';
 import PropertyList from '../../components/PropertyList/PropertyList';
 import './Home.css';
@@ -20,6 +21,7 @@ const Home = () => {
                Homes guests love
                </h1>
                <FeatureProperty/>
+               <MailList/>
            </div>
         </div>
     );
