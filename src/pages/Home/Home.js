176d1d11d2d185440357f90064b788b9b494efc3
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-07-20 01:10:29 +0600

    footer component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Featured from '../../components/Featured/Featured';
 import FeatureProperty from '../../components/FeatureProperty/FeatureProperty';
+import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 import MailList from '../../components/MailList/MailList';
 import Navbar from '../../components/Navbar/Navbar';
@@ -22,6 +23,7 @@ const Home = () => {
                </h1>
                <FeatureProperty/>
                <MailList/>
+               <Footer/>
            </div>
         </div>
     );
