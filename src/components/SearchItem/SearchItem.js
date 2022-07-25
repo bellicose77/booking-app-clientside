1c1b6405e9b9fead62b957771bb636ebddc983c5
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-07-25 21:28:31 +0600

    search ittem

--- src/components/SearchItem/SearchItem.js
+++ src/components/SearchItem/SearchItem.js
@@ -3,7 +3,7 @@ import './SearchItem.css';
 const SearchItem = () => {
     return (
         <div className="searchItem">
-            
+            <img src='https://cf.bstatic.com/xdata/images/hotel/square600/261707778.webp?k=fa6b6128468ec15e81f7d076b6f2473fa3a80c255582f155cae35f9edbffdd78&o=&s=1'/>
         </div>
     );
 };
