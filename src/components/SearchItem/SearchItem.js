4a41818ff836b1dd01c7985ab7658aeb815739c7
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-07-25 21:52:36 +0600

    serach options

--- src/components/SearchItem/SearchItem.js
+++ src/components/SearchItem/SearchItem.js
@@ -8,11 +8,11 @@ const SearchItem = () => {
             />
             <div className='siDesc'>
             <h1 className="siTitle">Tower Street Apartments</h1>
-        <span className="siDistance">500m from center</span>
-        <span className="siTaxiOp">Free airport taxi</span>
-        <span className="siSubtitle">
+            <span className="siDistance">500m from center</span>
+            <span className="siTaxiOp">Free airport taxi</span>
+            <span className="siSubtitle">
           Studio Apartment with Air conditioning
-        </span>
+             </span>
         <span className="siFeatures">
           Entire studio • 1 bathroom • 21m² 1 full bed
         </span>
