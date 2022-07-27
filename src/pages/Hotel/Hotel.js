0cf68995b7b20df70062a6c4084e0354d3eb565d
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-07-28 01:37:15 +0600

    slider container

--- src/pages/Hotel/Hotel.js
+++ src/pages/Hotel/Hotel.js
@@ -47,7 +47,7 @@ const Hotel = () => {
          <div className='hotelContainer'>
           {
             openSlide && <div className='sliderContainer'>
-              <FontAwesomeIcon icon={faCircleXmark} onClick={} className='close'/>
+              <FontAwesomeIcon icon={faCircleXmark} onClick={()=>setOpenSlide(true)} className='close'/>
               <FontAwesomeIcon icon={faCircleArrowLeft} className='arrow'/>
               <div className="sliderWrapper">
               <img src={photos[slideindex].src} alt="" className="sliderImg" />
