5b2af21b4d80772144211752258b93901b1bf0b6
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-07-28 00:10:15 +0600

    slider container

--- src/pages/Hotel/Hotel.js
+++ src/pages/Hotel/Hotel.js
@@ -45,7 +45,11 @@ const Hotel = () => {
            <Navbar/>
            <Header type="list"/>
          <div className='hotelContainer'>
-          <div className='sliderContainer'>slider</div>
+          {
+            openSlide && <div className='sliderContainer'>slider
+            </div>
+          }
+          
             <div className='hotelWrapper'>
             <button className="bookNow">Reserve or Book Now!</button>
                 <h1 className="hotelTitle">Tower Street Apartments</h1>
