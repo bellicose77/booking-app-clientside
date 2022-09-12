2490ceb668e390cf4e5e49195e8e325f85927059
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-12 14:24:46 +0600

    usefetch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,6 +1,8 @@
 import {useState} from 'react'
 
 const useFetch = (url)=>{
-const [data,setData] = useState([])
+const [data,setData] = useState([]);
+const [loading,setLoading] = useState(false)
+const [error,setError] = useState(false)
 }
 export default useFetch;
