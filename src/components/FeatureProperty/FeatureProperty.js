c87b61b603a36a0c452fb02924b137fe4944262e
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-12 16:55:52 +0600

    featured api integrate

--- src/components/FeatureProperty/FeatureProperty.js
+++ src/components/FeatureProperty/FeatureProperty.js
@@ -12,16 +12,20 @@ const FeatureProperty = () => {
                     <>
                     {
                         data.map((item)=>(
-                            <div className='fpItem' key={item.id}>
+                            <div className='fpItem' key={item._id}>
                            <img src='https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1'
                            className='fpImg'/>
                   <span className="fpName">{item.name}</span>
                 <span className="fpCity">{item.city}</span>
                 <span className="fpPrice">Starting from ${item.chepestPrice}</span>
-                <div className='fpRating'>
-                    <button>8.9</button>
+                {
+                    item.rating && <div className='fpRating'>
+    
+                    <button>{item.rating}</button>
                     <span>Excellent</span>
                 </div>
+                }
+                
 
             </div>
                         ))
