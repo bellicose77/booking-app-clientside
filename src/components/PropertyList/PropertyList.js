9e21ab135a419645b07397424d9f4dfe61c67530
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-12 15:33:48 +0600

    type api integrate done

--- src/components/PropertyList/PropertyList.js
+++ src/components/PropertyList/PropertyList.js
@@ -13,15 +13,27 @@ const PropertyList = () => {
   ];
     return (
         <div className='pLcontainer'>
-            <div className='pLitem'>
-                <img className='pLimg'
-                src='https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o='
-                />
+            {
+                loading ? ("Loading") :(
+                    <>
+                    {
+                        data && images.map((img,i)=>(
+                            <div className='pLitem' key={i}>
+
+                            <img className='pLimg'
+                              src={img}
+                             />
                 <div className='pLtitle'>
-                   <h1>Hotels</h1>
-                   <h2>87342 hotels</h2>
+                   <h1>{data[i]?.type}</h1>
+                   <h2>{data[i]?.count}</h2>
                 </div>
-            </div>    
+            </div> 
+                        ))
+                    }
+                    </>
+                )
+            }
+               
         </div>
     );
 };
