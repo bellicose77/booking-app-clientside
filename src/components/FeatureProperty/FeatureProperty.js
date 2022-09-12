a298ec573c3625fdb2b6cef78c74c73a1d495927
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-12 16:49:19 +0600

    featured api integrate

--- src/components/FeatureProperty/FeatureProperty.js
+++ src/components/FeatureProperty/FeatureProperty.js
@@ -3,45 +3,16 @@ import useFetch from '../../hooks/useFetch';
 import './FeatureProperty.css'
 
 const FeatureProperty = () => {
-    const {data,loading,error}=useFetch("")
+    const {data,loading,error}=useFetch("http://localhost:8000/api/hotel?featured=true");
+    console.log(data)
     return (
         <div className='fp'>
-            <div className='fpItem'>
-                <img src='https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1'
-                className='fpImg'/>
-                <span className="fpName">Aparthotel Stare Miasto</span>
-                <span className="fpCity">Mardid</span>
-                <span className="fpPrice">Starting from $120</span>
-                <div className='fpRating'>
-                    <button>8.9</button>
-                    <span>Excellent</span>
-                </div>
-
-            </div>
-            <div className='fpItem'>
-                <img src='https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1'
-                className='fpImg'/>
-                <span className="fpName">Aparthotel Stare Miasto</span>
-                <span className="fpCity">Mardid</span>
-                <span className="fpPrice">Starting from $120</span>
-                <div className='fpRating'>
-                    <button>8.9</button>
-                    <span>Excellent</span>
-                </div>
-
-            </div>
-            <div className='fpItem'>
-                <img src='https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1'
-                className='fpImg'/>
-                <span className="fpName">Aparthotel Stare Miasto</span>
-                <span className="fpCity">Mardid</span>
-                <span className="fpPrice">Starting from $120</span>
-                <div className='fpRating'>
-                    <button>8.9</button>
-                    <span>Excellent</span>
-                </div>
-
-            </div>
+            {
+                loading ? ("loading") : (
+                    <>
+                    </>
+                )
+            }
             <div className='fpItem'>
                 <img src='https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1'
                 className='fpImg'/>
