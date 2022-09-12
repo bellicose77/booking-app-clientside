9a10d0871859690314d057b0691c6ef4ec8dc9e4
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-12 16:58:01 +0600

    featured done

--- src/components/FeatureProperty/FeatureProperty.js
+++ src/components/FeatureProperty/FeatureProperty.js
@@ -13,7 +13,7 @@ const FeatureProperty = () => {
                     {
                         data.map((item)=>(
                             <div className='fpItem' key={item._id}>
-                           <img src='https://cf.bstatic.com/xdata/images/hotel/square600/13125860.webp?k=e148feeb802ac3d28d1391dad9e4cf1e12d9231f897d0b53ca067bde8a9d3355&o=&s=1'
+                           <img src={item.photos[0]}
                            className='fpImg'/>
                   <span className="fpName">{item.name}</span>
                 <span className="fpCity">{item.city}</span>
