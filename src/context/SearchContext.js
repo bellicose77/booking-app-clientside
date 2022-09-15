5324fa0ed4d8040e5af8b58221ae5cacf405b412
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-15 12:43:05 +0600

    context api

--- src/context/SearchContext.js
+++ src/context/SearchContext.js
@@ -32,4 +32,17 @@ const SearchReducer = (state,action) =>{
 export const SearchContextProvider = ({children}) =>{
     const [state,dispatch] = useReducer(SearchReducer,INITIAL_STATE);
 
+    return (
+        <SearchContext.Provider 
+          value={{
+            city: state.city,
+        dates: state.dates,
+        options: state.options,
+        dispatch,
+          }}
+        >
+            {children}
+        </SearchContext.Provider>
+    )
+
 }
