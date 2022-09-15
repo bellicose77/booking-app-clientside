b0659e01f807c3e4f9541fa06b17f4a3487d1bb0
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-15 13:16:16 +0600

    context api

--- src/pages/Hotel/Hotel.js
+++ src/pages/Hotel/Hotel.js
@@ -23,7 +23,13 @@ const Hotel = () => {
   const {data,loading,error} = useFetch(`http://localhost:8000/api/hotel/find/${id}`);
 
   const {dates} = useContext(SearchContext);
-  console.log(dates);
+  const MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24;
+  function dayDifference(date1, date2) {
+    const timeDiff = Math.abs(date2.getTime() - date1.getTime());
+    const diffDays = Math.ceil(timeDiff / MILLISECONDS_PER_DAY);
+    return diffDays;
+  }
+  //console.log(dates);
   //console.log(data)
 
   const handleOpen = (i) =>{
