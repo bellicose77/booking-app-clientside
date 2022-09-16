ff2782cc50cccb0068a455c01a8ff4dd5bd25958
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-16 18:42:58 +0600

    reverse componet

--- src/components/Reverse/Reverse.js
+++ src/components/Reverse/Reverse.js
@@ -1,8 +1,16 @@
+import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 
-const Reverse = ({setOpen,hotelId}) => {
+const Reverse = ({setOpenSlide,hotelId}) => {
     return (
-        <div>
+        <div className='reverse'>
+            <div className='rContainer'>
+               <FontAwesomeIcon 
+               icon={faCircleXmark}
+               className='rClose'
+               onClick={()=>setOpenSlide(false)}/>
+            </div>
             <h2>Reverse</h2>
         </div>
     );
