83d84ccf1bffab8c74ccaa9929f0210954b57d28
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-16 19:40:26 +0600

    integrate room api

--- src/components/Reverse/Reverse.js
+++ src/components/Reverse/Reverse.js
@@ -1,8 +1,11 @@
 import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import useFetch from '../../hooks/useFetch';
 
 const Reverse = ({setOpenSlide,hotelId}) => {
+    const{data,loading,error} = useFetch(`http://localhost:8000/api/hotel/room/${hotelId}`);
+    console.log(data)
     return (
         <div className='reverse'>
             <div className='rContainer'>
@@ -11,6 +14,13 @@ const Reverse = ({setOpenSlide,hotelId}) => {
                className='rClose'
                onClick={()=>setOpenSlide(false)}/>
                <span>Select your rooms:</span>
+               {
+                data.map((item)=>(
+                <div className="rItem" key={item._id}>
+
+                </div>
+                ))
+               }
             </div>
 
         </div>
