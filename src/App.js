399433208bdfd276a7bd915a6f692ff34e9eb4ee
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-16 16:22:08 +0600

    login page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Home from './pages/Home/Home';
 import Hotel from './pages/Hotel/Hotel';
 import List from './pages/List/List';
+import Login from './pages/Login/Login';
 function App() {
   return (
     <div>
@@ -10,6 +11,7 @@ function App() {
         <Route path='/' element={<Home/>}/>
         <Route path='/hotels/:id' element={<Hotel/>}/>
         <Route path='/hotels' element={<List/>}/>
+        <Route path='/login' element={<Login/>}/>
       </Routes>
      </Router>
     </div>
