d257e0e2902216fedbfff93ade1502f527b8f7b9
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-16 15:00:08 +0600

    auth context

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { SearchContextProvider } from './context/SearchContext';
+import { AuthContext } from './context/AuthContext';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <AuthContext>
     <SearchContextProvider>
     <App />
     </SearchContextProvider>
+    </AuthContext>
+   
     
   </React.StrictMode>
 );
