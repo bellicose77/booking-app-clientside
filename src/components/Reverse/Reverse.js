33a9d653d430e8ee63bb0c6e34c79bc2fc017327
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-16 19:49:37 +0600

    integrate room api

--- src/components/Reverse/Reverse.js
+++ src/components/Reverse/Reverse.js
@@ -17,7 +17,29 @@ const Reverse = ({setOpenSlide,hotelId}) => {
                {
                 data.map((item)=>(
                 <div className="rItem" key={item._id}>
-
+                  <div className='rItemInfo'>
+                     <div className="rTitle">{item.title}</div>
+                     <div className="rDesc">{item.desc}</div>
+                     <div className="rMax">
+                       Max people: <b>{item.maxPeople}</b>
+                     </div>
+                     <div className="rPrice">{item.price}</div>
+                  </div>
+                  <div className='rSelectRooms'>
+                     {
+                        item.roomNumbers.map((roomNumber)=>(
+                            <div className='room'>
+                                <label>{roomNumber.number}</label>
+                  <input
+                    type="checkbox"
+                    value={roomNumber._id}
+                    onChange={handleSelect}
+                    //disabled={!isAvailable(roomNumber)}
+                  />
+                             </div>
+                        ))
+                     }
+                  </div>
                 </div>
                 ))
                }
