d852121a805f242f08305eabceb05b3d110f62d5
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-16 18:31:48 +0600

    reverse componet

---
+++ src/components/Reverse/Reverse.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Reverse = ({setOpen,hotelId}) => {
+    return (
+        <div>
+            <h2>Reverse</h2>
+        </div>
+    );
+};
+
+export default Reverse;
--- src/pages/Hotel/Hotel.js
+++ src/pages/Hotel/Hotel.js
@@ -129,6 +129,7 @@ const Hotel = () => {
             <Footer/>
            </div>
            )}
+           {openModal && }
     </div>
     );
 };
